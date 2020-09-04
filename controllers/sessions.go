5d749e0bda5fefce5ddf5e2f713c793df76b9d3c
PhysPeach <Masato.Shukawa@outlook.jp>
2020-09-04 11:51:48 +0900

    create close #37

--- controllers/sessions.go
+++ controllers/sessions.go
@@ -36,12 +36,12 @@ func (c *SessionsController) New() {
 // @Failure 403 body is empty
 // @router / [post]
 func (c *SessionsController) Create() {
-	isCorrect, account := ConfirmAccountPassword(c)
-	if isCorrect {
+	account, err := ConfirmAccountPassword(c)
+	if err == nil {
 		c.SetSession("sessAccountID", account.ID)
 		c.Ctx.Redirect(302, "/" + account.Name)
 	}
-	c.Data["loginError"] = errors.New("Wrong name or password")
+	c.Data["loginError"] = err
 	c.Data["xsrf"] = template.HTML(c.XSRFFormHTML())
 	c.Layout = "layouts/application.tpl"
 	c.TplName = "sessions/new.tpl"
@@ -52,19 +52,19 @@ func (c *SessionsController) Destroy() {
 	c.Ctx.Redirect(302, "/")
 }
 
-func ConfirmAccountPassword(c *SessionsController)(bool, *models.Account){
+func ConfirmAccountPassword(c *SessionsController)(*models.Account, error){
 	accountName := c.GetString("Name")
 	unhashed := c.GetString("Password")
 	account, err := models.GetAccountByName(accountName)
 	if err != nil{
-		return false, account
+		return nil, errors.New(accountName + " could not be found")
 	}
 	hashed, err := hex.DecodeString(account.Password)
 	if err != nil{
 		c.Abort("500")
 	}
 	if err := bcrypt.CompareHashAndPassword(hashed, []byte(unhashed)); account.Password != "0123" && err != nil {
-		return false, account
+		return nil, errors.New("Wrong password")
 	}
-	return (account.ID != 0), account
+	return account, nil
 }
